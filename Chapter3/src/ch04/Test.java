849d5528a66e93e92a85022a3933110c4f1eb332
ken <tlap@naver.com>
2024-03-29 02:05:59 +0900

    javaReView

--- Chapter3/src/ch04/Test.java
+++ Chapter3/src/ch04/Test.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String c = "";
         
-        for (int i=0; i<a.length(); i++){
-            int b = a.charAt(i);
-            if (b >= 65 && b <= 90){
-                c += (char)Character.toLowerCase(b);
-            } else if (b >= 97 && b <= 122){
-                c += (char)Character.toUpperCase(b);
-            }
-        }
-        
-        System.out.print(c);
-    
-    }
+
+
+		System.out.println(solution("He11oWor1d", "lloWorl", 2));
+		System.out.println(solution("Program29b8UYP", "merS123", 7));
+		// replace 했을 경우 원하는 부분이 아니라 똑같은 부분만 전부 바뀔수 있다.
+	
+	
+	}
+	public static String solution(String my_string, String overwrite_string, int s) {
+		String answer = "";
+		String a = "";
+		
+		a = my_string.substring(s, overwrite_string.length() + s);
+		answer = my_string.replace(a, overwrite_string);
+		
+		return answer;
+//		return a;
+	}
 }
+
+
